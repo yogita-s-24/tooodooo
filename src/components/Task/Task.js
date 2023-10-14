9f2d7f3dcdfb9544a13c9961c9df9f5047336cd8
yogita-s-24 <yogitashete24@gmail.com>
2023-10-14 10:26:25 +0530

    add task completed

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Task.css'
 
-const Task = () => {
+const Task = ({id, title, description, priority}) => {
   return (
-    <div>
-      
+    <div className='card-container'>
+      <p className='random-id'>{id}</p>
+      <p className='card-title'>{title}</p>
+      <p className='card-description'>{description}</p>
+      <p className='card-priority'>{priority}</p>
+
+      <div className='remove-update-task'>
+      <span className='remove-task'>❌</span>
+      <img src="https://cdn-icons-png.flaticon.com/128/227/227104.png" height="20px" className='update-task'/>
+
+      </div>
     </div>
   )
 }
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
+import Task from "./../../components/Task/Task";
 
 const Home = () => {
+  const [id, setId] = useState(0);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [priority, setPriority] = useState("");
+  const [taskList, setTaskList] = useState([
+    {
+      id: "1",
+      title: "Assignment",
+      description: "In 2 days",
+      priority: "high",
+    },
+  ]);
+
+  const addTaskList = () => {
+    let randomId = Math.floor(Math.random() * 1000);
+    const object = {
+      id: randomId,
+      title: title,
+      description: description,
+      priority: priority,
+    };
+
+    const newarrayList = [...taskList, object];
+    setTaskList(newarrayList);
+  };
+
+
 
   return (
     <div>
@@ -9,13 +37,62 @@ const Home = () => {
       <p className="second-title display">
         <b>A simple to do list app that helps you keep track of your tasks.</b>
       </p>
+
       <div className="container">
         <div>
           <h1>Show List</h1>
-          
+
+          {taskList.map((taskitem, index) => {
+            const { id, tittle, description, priority } = taskitem;
+
+            return (
+              <Task
+                id={id}
+                tittle={tittle}
+                description={description}
+                priority={priority}
+                key={index}
+              />
+            );
+          })}
         </div>
         <div>
           <h1>Add List</h1>
+          <div className="add-container">
+            <input
+              type="text"
+              placeholder="Enter Title Here"
+              className="input-field"
+              onChange={(e) => {
+                setTitle(e.target.value);
+              }}
+            />
+            <br />
+            <br />
+            <input
+              type="text"
+              placeholder="Enter Description Here"
+              className="input-field"
+              onChange={(e) => {
+                setDescription(e.target.value);
+              }}
+            />
+            <br />
+            <br />
+            <input
+              type="text"
+              placeholder="Enter Priority Here"
+              className="input-field"
+              onChange={(e) => {
+                setPriority(e.target.value);
+              }}
+            />
+            <br />
+            <br />
+            <button type="submit" className="button" onClick={addTaskList}>
+              Add Task
+            </button>
+          </div>
         </div>
       </div>
     </div>
